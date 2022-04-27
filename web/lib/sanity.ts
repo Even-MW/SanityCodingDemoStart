97799af51b4701f520f1662b01c556b62f010eda
Even <even.mauland-hus@noaignite.com>
2022-04-27 15:17:12 +0200

    Cleaned up project

--- web/lib/sanity.ts
+++ web/lib/sanity.ts
@@ -1,7 +1,8 @@
-import { createImageUrlBuilder, createPreviewSubscriptionHook } from "next-sanity";
+import { createPreviewSubscriptionHook } from 'next-sanity'
+import imageUrlBuilder from '@sanity/image-url'
 import { sanityConfig } from "./config";
 
-export const imageBuilder = createImageUrlBuilder(sanityConfig);
+export const imageBuilder = imageUrlBuilder(sanityConfig);
 
 export const urlForImage = (source) => imageBuilder.image(source).auto("format").fit("max");
 
